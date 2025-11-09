3894ae13dea005458c81f9961679b7379e0a88ea
caitlynlie <caitlynl12@nycstudents.net>
2025-11-09 20:26:34 +0000

    f

--- VRLesson2_8Classwork/script.js
+++ VRLesson2_8Classwork/script.js
@@ -10,8 +10,11 @@ window.addEventListener("DOMContentLoaded",function() {
 
 function loop(){
   target.a += target.da;
-
+  let limi = Math.pi/2;
   target.setAttribute("rotation",{x:target.a, y:0, z: 0});
-
+  if(this.a > limi || this.a < 0){
+    this.da = -this.da;
+  }
+  target.setAttribute("rotation",{x:target.a, y:0, z: 0});
   window.requestAnimationFrame( loop );
 }
--- VRLesson2_9Classwork/script.js
+++ VRLesson2_9Classwork/script.js
@@ -1,16 +1,16 @@
 let maze = [
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
+  "----xxxxxxxxxxxxxxxx",
+  "----x--t----t------t----",
+  "-xxxx-x--xxxxxxxx--xxx",
+  "-x---o---xxoxxo--o---xxx",
+  "-x-x-xxxxx-xxxxxxxxxxx",
+  "-x-x-x-x---o---o----o---t",
+  "-xtx-o-x-xxoxxx-xxxxxx",
+  "-xxx-x-x-x----t----txox",
+  "-x-----x-t-x-x--xxxx-x",
+  "-x-xtx---x---x-------x",
+  "-xxxxxxxxxxxxxxxxxx--x",
+  "--t---t-t-----t---t----x--x",
 ];
 let scene;
 
@@ -23,7 +23,7 @@ window.addEventListener("DOMContentLoaded",function() {
       if(cols[c] == "x"){
         new Block(c,1,r)
       }
-      else if(cols[c] == "T"){
+      else if(cols[c] == "t"){
         new Tree(c,1,r);
       }else if(cols[c]=="o"){
         new Rock(c,1,r)
