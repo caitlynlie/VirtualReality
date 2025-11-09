7ed76a54848a711b618235c7cbf4d89fe281f860
caitlynlie <caitlynl12@nycstudents.net>
2025-11-09 20:15:08 +0000

    f

--- VRLesson2_6Classwork/script.js
+++ VRLesson2_6Classwork/script.js
@@ -1,10 +1,17 @@
-let scene;
+
+let scene, mechs = [], mech;
+let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   
   mech = document.getElementById("mech-1");
- 
+  for(let i = 0; i < 1; i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+    let m = new Mech(x, 0 , z);
+    mechs.push(m);
+  }
   loop();
 })
 function loop(){
